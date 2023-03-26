b8e1519b853bb827b12aa4d58d930b5053dc10b4
Rodrigo Castañón Martínez <71529686+KastaTM@users.noreply.github.com>
2023-03-26 01:02:11 +0100

    Add files via upload

---
+++ 116.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int numcasos;
+	cin >> numcasos;
+	for (int i = 0; i < numcasos; i++) {
+		cout << "Hola mundo." << endl;
+	}
+	return 0;
+}
