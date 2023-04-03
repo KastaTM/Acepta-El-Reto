c8c021d1336a52e615a7bfea6276b8fbcb4b8a4f
Rodrigo Castañón Martínez <71529686+KastaTM@users.noreply.github.com>
2023-04-03 13:21:04 +0200

    Add files via upload

---
+++ 165.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char numero[101];
+
+int main() {
+    int res;
+    int i;
+    while (1) {
+        scanf("%s", numero);
+        if (atoi(numero) < 0)
+            return 0;
+        res = 1;
+        for (i = 0; i < strlen(numero); i++) {
+            if ((numero[i] - '0') % 2)
+                res = 0;
+        }
+        if (res)
+            printf("SI\n");
+        else
+            printf("NO\n");
+    }
+}
